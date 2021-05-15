a1e90e7f9716308f4456b0430a3b7157ee8844cf
koyashiro <develop@koyashiro.net>
2021-05-16 03:18:09 +0900

    feat: add `IDirectoryWrapper.Exists()`

--- src/VRCLauncher/Wrappers/DirectoryWrapper.cs
+++ src/VRCLauncher/Wrappers/DirectoryWrapper.cs
@@ -4,6 +4,11 @@ namespace VRCLauncher.Wrappers
 {
     public class DirectoryWrapper : IDirectoryWrapper
     {
+        public bool Exists(string? path)
+        {
+            return Directory.Exists(path);
+        }
+
         public void CreateDirectory(string path)
         {
             Directory.CreateDirectory(path);
--- src/VRCLauncher/Wrappers/IDirectoryWrapper.cs
+++ src/VRCLauncher/Wrappers/IDirectoryWrapper.cs
@@ -2,6 +2,7 @@ namespace VRCLauncher.Wrappers
 {
     public interface IDirectoryWrapper
     {
+        bool Exists(string? path);
         void CreateDirectory(string path);
     }
 }
