e97904c7d0d7eea1111a21ee39d08517926a2882
koyashiro <develop@koyashiro.net>
2021-05-16 04:30:17 +0900

    fix: add `-` to nonce regex pattern

--- src/VRCLauncher/Models/LaunchParameter.cs
+++ src/VRCLauncher/Models/LaunchParameter.cs
@@ -9,7 +9,7 @@ namespace VRCLauncher.Models
         private const string REGEX_UUID = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
         private const string REGEX_WORLD_ID = "wrld_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
         private const string REGEX_USER_ID = "usr_[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
-        private const string REGEX_NONCE = "[0-9A-z]+";
+        private const string REGEX_NONCE = "[0-9A-z\\-]+";
 
         public string WorldId { get; set; } = string.Empty;
         public string InstanceId { get; set; } = string.Empty;
